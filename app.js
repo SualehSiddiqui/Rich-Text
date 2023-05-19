72ef48f7d2a681fca42cc0c3a73e8382a5e9f1eb
Sualeh Siddiqui <sualehmsiddiqui@gmail.com>
2023-05-20 01:29:28 +0500

    Rich Text completed
    Rich Text completed

--- app.js
+++ app.js
@@ -98,55 +98,55 @@ function forFontFamily() {
     }
 }
 
-function forFontSize(){
+function forFontSize() {
     var fontSizeValue = fontSize.options[fontSize.selectedIndex].value;
-    
-    if(fontSizeValue == 8){
+
+    if (fontSizeValue == 8) {
         bodyTxt.style.fontSize = "8px"
     }
-    else if(fontSizeValue == 9){
+    else if (fontSizeValue == 9) {
         bodyTxt.style.fontSize = "9px"
     }
-    else if(fontSizeValue == 10){
+    else if (fontSizeValue == 10) {
         bodyTxt.style.fontSize = "10px"
     }
-    else if(fontSizeValue == 11){
+    else if (fontSizeValue == 11) {
         bodyTxt.style.fontSize = "11px"
     }
-    else if(fontSizeValue == 12){
+    else if (fontSizeValue == 12) {
         bodyTxt.style.fontSize = "12px"
     }
-    else if(fontSizeValue == 14){
+    else if (fontSizeValue == 14) {
         bodyTxt.style.fontSize = "14px"
     }
-    else if(fontSizeValue == 16){
+    else if (fontSizeValue == 16) {
         bodyTxt.style.fontSize = "16px"
     }
-    else if(fontSizeValue == 18){
+    else if (fontSizeValue == 18) {
         bodyTxt.style.fontSize = "18px"
     }
-    else if(fontSizeValue == 20){
+    else if (fontSizeValue == 20) {
         bodyTxt.style.fontSize = "20px"
     }
-    else if(fontSizeValue == 22){
+    else if (fontSizeValue == 22) {
         bodyTxt.style.fontSize = "22px"
     }
-    else if(fontSizeValue == 24){
+    else if (fontSizeValue == 24) {
         bodyTxt.style.fontSize = "24px"
     }
-    else if(fontSizeValue == 26){
+    else if (fontSizeValue == 26) {
         bodyTxt.style.fontSize = "26px"
     }
-    else if(fontSizeValue == 28){
+    else if (fontSizeValue == 28) {
         bodyTxt.style.fontSize = "28px"
     }
-    else if(fontSizeValue == 36){
+    else if (fontSizeValue == 36) {
         bodyTxt.style.fontSize = "36px"
     }
-    else if(fontSizeValue == 48){
+    else if (fontSizeValue == 48) {
         bodyTxt.style.fontSize = "48px"
     }
-    else if(fontSizeValue == 72){
+    else if (fontSizeValue == 72) {
         bodyTxt.style.fontSize = "72px"
     }
 
@@ -155,13 +155,13 @@ function forFontSize(){
 var bold = document.getElementById("for-bold");
 bold.value = "";
 
-function forBold(){
-    if(bold.value == ""){
+function forBold() {
+    if (bold.value == "") {
         bodyTxt.style.fontWeight = "Bold";
         bold.style.backgroundColor = "#000";
         bold.innerHTML = "<i class='fa-solid fa-bold' style='color: #f0f2f5;'></i>";
         bold.value = "bold"
-    }else{
+    } else {
         bodyTxt.style.fontWeight = "400";
         bold.style.backgroundColor = "#fff";
         bold.innerHTML = "<i class='fa-solid fa-bold'></i>";
@@ -172,14 +172,14 @@ function forBold(){
 var italic = document.getElementById("for-italic");
 italic.value = "";
 
-function forItalic(){
-    if(italic.value == ""){
+function forItalic() {
+    if (italic.value == "") {
         console.log("if")
         bodyTxt.style.fontStyle = "italic";
         italic.style.backgroundColor = "#000";
         italic.innerHTML = "<i class='fa-solid fa-italic' style='color: #f0f2f5;'></i>";
         italic.value = "italic"
-    }else{
+    } else {
         console.log("else")
         bodyTxt.style.fontStyle = "normal";
         italic.style.backgroundColor = "#fff";
@@ -191,14 +191,14 @@ function forItalic(){
 var underLine = document.getElementById("for-underline");
 underLine.value = "";
 
-function forUnderLine(){
-    if(underLine.value == ""){
+function forUnderLine() {
+    if (underLine.value == "") {
         console.log("if")
         bodyTxt.style.textDecoration = "underline";
         underLine.style.backgroundColor = "#000";
         underLine.innerHTML = "<i class='fa-solid fa-underline' style='color: #f0f2f5;'></i>";
         underLine.value = "underLine"
-    }else{
+    } else {
         console.log("else")
         bodyTxt.style.textDecoration = "none";
         underLine.style.backgroundColor = "#fff";
@@ -210,14 +210,14 @@ function forUnderLine(){
 var strikeThrough = document.getElementById("for-strike");
 strikeThrough.value = "";
 
-function forStrikeThrough(){
-    if(strikeThrough.value == ""){
+function forStrikeThrough() {
+    if (strikeThrough.value == "") {
         console.log("if")
         bodyTxt.style.textDecoration = "line-through";
         strikeThrough.style.backgroundColor = "#000";
         strikeThrough.innerHTML = "<i class='fa-solid fa-strikethrough' style='color: #f0f2f5;'></i>";
         strikeThrough.value = "strikeThrough"
-    }else{
+    } else {
         console.log("else")
         bodyTxt.style.textDecoration = "none";
         strikeThrough.style.backgroundColor = "#fff";
@@ -226,26 +226,172 @@ function forStrikeThrough(){
     }
 }
 
-var forCase = document.getElementById("for-case");
-forCase.value = "";
+var capitalize = document.getElementById("for-capitalize");
+capitalize.value = "";
 
-function forCase(){
-    if(forCase.value == ""){
+function forCapitalize() {
+    if (capitalize.value == "") {
         console.log("if")
         bodyTxt.style.textTransform = "capitalize";
-        forCase.style.backgroundColor = "#000";
-        forCase.innerHTML = "<i class='fa-solid fa-font-case' style='color: #f0f2f5;'></i>";
-        forCase.value = "forCase"
-    }else{
+        capitalize.value = "forcapitalize"
+    } else {
         console.log("else")
         bodyTxt.style.textTransform = "none";
-        forCase.style.backgroundColor = "#fff";
-        forCase.innerHTML = "<i class='fa-solid fa-font-case'></i>";
-        forCase.value = "";
+        capitalize.value = "";
+    }
+}
+
+var upperCase = document.getElementById("for-uppercase");
+upperCase.value = "";
+
+function forUpperCase() {
+    if (upperCase.value == "") {
+        console.log("if")
+        bodyTxt.style.textTransform = "uppercase";
+        upperCase.value = "forUppercase"
+    } else {
+        console.log("else")
+        bodyTxt.style.textTransform = "none";
+        upperCase.value = "";
+    }
+}
+
+var lowerCase = document.getElementById("for-lowercase");
+lowerCase.value = "";
+
+function forLowerCase() {
+    if (lowerCase.value == "") {
+        console.log("if")
+        bodyTxt.style.textTransform = "lowercase";
+        lowerCase.value = "forLowercase"
+    } else {
+        console.log("else")
+        bodyTxt.style.textTransform = "none";
+        lowerCase.value = "";
+    }
+}
+
+var alignRight = document.getElementById("for-align-right");
+alignRight.value = "";
+
+function forAlignRight() {
+    if (alignRight.value == "") {
+        //Right
+        bodyTxt.style.textAlign = "right";
+        alignRight.style.backgroundColor = "#000";
+        alignRight.innerHTML = "<i class='fa-solid fa-align-right' style='color: #f0f2f5;'></i>";
+        alignRight.value = "alignRight"
+        //Center
+        alignCenter.style.backgroundColor = "#fff";
+        alignCenter.innerHTML = "<i class='fa-solid fa-align-center'></i>";
+        alignCenter.value = "";
+        //Left
+        alignLeft.style.backgroundColor = "#fff";
+        alignLeft.innerHTML = "<i class='fa-solid fa-align-left'></i>";
+        alignLeft.value = "";
+        //Justify
+        alignJustify.style.backgroundColor = "#fff";
+        alignJustify.innerHTML = "<i class='fa-solid fa-align-justify'></i>";
+        alignJustify.value = "";
+    } else {
+        bodyTxt.style.textAlign = "start";
+        alignRight.style.backgroundColor = "#fff";
+        alignRight.innerHTML = "<i class='fa-solid fa-align-right'></i>";
+        alignRight.value = "";
+    }
+}
+
+var alignLeft = document.getElementById("for-align-left");
+alignLeft.value = "";
+
+function forAlignLeft() {
+    if (alignLeft.value == "") {
+        //Left
+        bodyTxt.style.textAlign = "left";
+        alignLeft.style.backgroundColor = "#000";
+        alignLeft.innerHTML = "<i class='fa-solid fa-align-left' style='color: #f0f2f5;'></i>";
+        alignLeft.value = "alignLeft"
+        //Center
+        alignCenter.style.backgroundColor = "#fff";
+        alignCenter.innerHTML = "<i class='fa-solid fa-align-center'></i>";
+        alignCenter.value = "";
+        //Right
+        alignRight.style.backgroundColor = "#fff";
+        alignRight.innerHTML = "<i class='fa-solid fa-align-right'></i>";
+        alignRight.value = "";
+        //Justify
+        alignJustify.style.backgroundColor = "#fff";
+        alignJustify.innerHTML = "<i class='fa-solid fa-align-justify'></i>";
+        alignJustify.value = "";
+    } else {
+        bodyTxt.style.textAlign = "start";
+        alignLeft.style.backgroundColor = "#fff";
+        alignLeft.innerHTML = "<i class='fa-solid fa-align-left'></i>";
+        alignLeft.value = "";
+    }
+}
+
+var alignJustify = document.getElementById("for-align-justify");
+alignJustify.value = "";
+
+function forAlignJustify() {
+    if (alignJustify.value == "") {
+        //justify
+        bodyTxt.style.textAlign = "justify";
+        alignJustify.style.backgroundColor = "#000";
+        alignJustify.innerHTML = "<i class='fa-solid fa-align-justify' style='color: #f0f2f5;'></i>";
+        alignJustify.value = "alignJustify"
+        //Center
+        alignCenter.style.backgroundColor = "#fff";
+        alignCenter.innerHTML = "<i class='fa-solid fa-align-center'></i>";
+        alignCenter.value = "";
+        //Left
+        alignLeft.style.backgroundColor = "#fff";
+        alignLeft.innerHTML = "<i class='fa-solid fa-align-left'></i>";
+        alignLeft.value = "";
+        //Right
+        alignRight.style.backgroundColor = "#fff";
+        alignRight.innerHTML = "<i class='fa-solid fa-align-right'></i>";
+        alignRight.value = "";
+    } else {
+        bodyTxt.style.textAlign = "start";
+        alignJustify.style.backgroundColor = "#fff";
+        alignJustify.innerHTML = "<i class='fa-solid fa-align-justify'></i>";
+        alignJustify.value = "";
+    }
+}
+
+var alignCenter = document.getElementById("for-align-center");
+alignCenter.value = "";
+
+function forAlignCenter() {
+    if (alignCenter.value == "") {
+        //Center
+        bodyTxt.style.textAlign = "center";
+        alignCenter.style.backgroundColor = "#000";
+        alignCenter.innerHTML = "<i class='fa-solid fa-align-center' style='color: #f0f2f5;'></i>";
+        alignCenter.value = "alignCenter"
+        //Left
+        alignLeft.style.backgroundColor = "#fff";
+        alignLeft.innerHTML = "<i class='fa-solid fa-align-left'></i>";
+        alignLeft.value = "";
+        //Right
+        alignRight.style.backgroundColor = "#fff";
+        alignRight.innerHTML = "<i class='fa-solid fa-align-right'></i>";
+        alignRight.value = "";
+        //Justify
+        alignJustify.style.backgroundColor = "#fff";
+        alignJustify.innerHTML = "<i class='fa-solid fa-align-justify'></i>";
+        alignJustify.value = "";
+    } else {
+        bodyTxt.style.textAlign = "start";
+        alignCenter.style.backgroundColor = "#fff";
+        alignCenter.innerHTML = "<i class='fa-solid fa-align-center'></i>";
+        alignCenter.value = "";
     }
 }
 
 var fontColor = document.getElementById("font-color");
-function forFontColor(){
+function forFontColor() {
     bodyTxt.style.color = fontColor.value;
 }
